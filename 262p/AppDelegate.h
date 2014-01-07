17aa89d46cd620494fafc992024d58ec989e9099
SeungyoupBaek <bsy8220@naver.com>
2014-01-08 02:34:45 +0900

    Initial Commit

---
+++ 262p/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  262p
+//
+//  Created by Youp on 2014. 1. 8..
+//  Copyright (c) 2014년 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
